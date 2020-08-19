3499181501ae87c3655cfff52d041694eae33025
Hayk <hayk.hayr3@gmail.com>
2020-08-20 03:01:21 +0400

    Add project files.

---
+++ Palindrome/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a word: ");
+            string inputString = Console.ReadLine();
+            if(checkPalindrome(inputString))
+                Console.WriteLine($"{inputString} word is palindrome!");
+            else
+                Console.WriteLine($"{inputString} word is not palindrome!");
+
+            Console.ReadLine();
+        }
+        static bool checkPalindrome(string inputString)
+        {
+            for (int i = 0; i < inputString.Length / 2; i++)
+            {
+                if (inputString[i] != inputString[inputString.Length - 1 - i])
+                    return false;
+            }
+            return true;
+        }
+
+    }
+}
